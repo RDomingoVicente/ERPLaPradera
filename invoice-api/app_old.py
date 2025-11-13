2cc925f8a7f08fa54ae68fb7791b410afae5c44e
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-13 20:57:46 +0100

    intro de prueba

--- invoice-api/app_old.py
+++ invoice-api/app_old.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # Define la ruta a tu carpeta de plantillas
 custom_template_path = '../MisTemplates'
 
+
 # Cargar templates de invoice2data
 templates = read_templates(folder=custom_template_path)
 

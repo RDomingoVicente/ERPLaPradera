fc3bba6afbd73a3c9c56f84599aad8ae2eae3916
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-15 16:40:56 +0100

    Este es el commit bueno

--- ejecutaInvoice2data.sh
+++
@@ -1,2 +0,0 @@
-invoice2data  --debug --template-folder $HOME/invoice2data/MisTemplates --exclude-built-in-templates --input-reader pdftotext  --output-format json \
---output-name $HOME/invoice2data/MisOutput/tudis $HOME/invoice2data/MisFacturas/002741M5234420251031.pdf

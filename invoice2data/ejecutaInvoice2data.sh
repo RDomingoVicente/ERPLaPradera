74796e1a6d9893f7b2d91ce73fb1748ce60a5ed5
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-15 16:36:50 +0100

    fichero de salida json y eliminar script ejecutar del raiz

--- invoice2data/ejecutaInvoice2data.sh
+++ invoice2data/ejecutaInvoice2data.sh
@@ -1,2 +1,2 @@
-invoice2data  --debug --template-folder $HOME/invoice2data/MisTemplates --exclude-built-in-templates --input-reader pdftotext  --output-format csv \
---output-name $HOME/invoice2data/MisOutput/tudis $HOME/invoice2data/MisFacturas/002741M5234420251031.pdf
+invoice2data  --debug --template-folder ./MisTemplates --exclude-built-in-templates --input-reader pdftotext  --output-format json \
+--output-name ./MisOutput/tudis ./MisFacturas/002741M5234420251031.pdf

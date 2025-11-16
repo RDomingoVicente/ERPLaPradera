e075b8c01a78f90fd628cb184d35dfdbcd478ecb
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-16 17:35:47 +0100

    nueva template para La Calle

---
+++ invoice2data/ejecutaInvoice2data_LaCalle.sh
@@ -0,0 +1,3 @@
+invoice2data  --debug --template-folder ./MisTemplates/lacalle --exclude-built-in-templates --input-reader pdftotext  --output-format json \
+--output-name ./MisOutput/LaCalle/VF01-250001142.json ./MisFacturas/VF01-250001142.pdf
+

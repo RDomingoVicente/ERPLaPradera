b31f77800b7ba93ed135988b3b321e7b508ed526
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-16 21:26:28 +0100

    nueva template para Bebidas Martin

---
+++ invoice2data/ejecutaInvoice2data_Martin.sh
@@ -0,0 +1,3 @@
+invoice2data  --debug --template-folder ./MisTemplates/BebidasMartin --exclude-built-in-templates --input-reader pdftotext  --output-format json \
+--output-name ./MisOutput/BebidasMartin/'PRADERA FRA 127713.json' ./MisFacturas/'PRADERA FRA 127713.pdf'
+

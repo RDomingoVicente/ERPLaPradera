846eea93020f80b62809f9206153e7a011ef9238
Ricardo Domingo <ricardo.domingo66@gmail.com>
2025-11-17 16:42:22 +0100

    Cambios menores

--- invoice2data/ejecutaInvoice2data_GM.sh
+++ invoice2data/ejecutaInvoice2data_GM.sh
@@ -1,5 +1,5 @@
 invoice2data  --debug --template-folder ./MisTemplates/GM --exclude-built-in-templates --input-reader pdftotext  --output-format json \
---output-name ./MisOutput/GM/FAC_088049872.json ./MisFacturas/FAC_088049872.pdf
+--output-name ./MisOutput/GM/FAC_088058828.json ./MisFacturas/FAC_088058828.pdf
 
 
 #invoice2data --input-reader pdfplumber --debug --template-folder ./MisTemplates/GM  --exclude-built-in-templates ./MisFacturas/FAC_088049872.pdf --output-format json \
